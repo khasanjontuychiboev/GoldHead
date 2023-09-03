a9c7a59fac83b601d7741bf0817bb68d399886e3
Xasanboy <ucbeyibenim@gmail.com>
2023-09-03 23:06:18 +0500

    country controller just created

--- src/main/java/com/akaxon/goldhead/country/CountryController.java
+++ src/main/java/com/akaxon/goldhead/country/CountryController.java
@@ -1,4 +1,14 @@
 package com.akaxon.goldhead.country;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/country")
 public class CountryController {
+    @GetMapping
+    public void getAllCountries(){
+        System.out.println("hello countries!");
+    }
 }
