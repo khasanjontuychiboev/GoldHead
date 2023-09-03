576ecc31ec5bd5e6304729e4677ee71f827ad9fd
Xasanboy <ucbeyibenim@gmail.com>
2023-09-03 23:12:52 +0500

    country controller's methods were just created

--- src/main/java/com/akaxon/goldhead/country/CountryController.java
+++ src/main/java/com/akaxon/goldhead/country/CountryController.java
@@ -1,6 +1,7 @@
 package com.akaxon.goldhead.country;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,9 @@ public class CountryController {
     @GetMapping
     public void getAllCountries(){
         System.out.println("hello countries!");
+    }
+    @GetMapping(path = "/{id}")
+    public void getCountry(@PathVariable("id") String id){
+        System.out.println("One Country By ID");
     }
 }
